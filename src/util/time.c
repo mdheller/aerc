1d41c3d2283d9f4afb9fb36a30a5f46ab2dd5482
Drew DeVault <sir@cmpwn.com>
2016-10-08 21:26:28 -0400

    Deindent preprocessor directives

--- src/util/time.c
+++ src/util/time.c
@@ -9,7 +9,7 @@
 
 void get_nanoseconds(struct timespec *tp) {
 	// http://stackoverflow.com/questions/5167269/clock-gettime-alternative-in-mac-os-x#6725161
-	#ifdef __MACH__
+#ifdef __MACH__
 	clock_serv_t cclock;
 	mach_timespec_t mts;
 	host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
@@ -17,9 +17,9 @@ void get_nanoseconds(struct timespec *tp) {
 	mach_port_deallocate(mach_task_self(), cclock);
 	tp->tv_sec = mts.tv_sec;
 	tp->tv_nsec = mts.tv_nsec;
-	#else
+#else
 	clock_gettime(CLOCK_MONOTONIC, tp);
-	#endif
+#endif
 
 	(void) tp;
 }
