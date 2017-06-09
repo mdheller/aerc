mdheller/aerc

commit 5736cb57863c261b86b38588eb50f4ead0e7b80b

Handle flag change on unfetched messages
With multiple IMAP accounts, the worker for the second account will
connect, issue an IMAP SELECT for "INBOX", but not actually fetch the
headers of messages. If another client on this account updates the
flags on a message, the server will send an IMAP FETCH. The fetch
handler assumed that whenever the server sent a FETCH, the message
would be fully populated, which is incorrect in this case.

To handle this, we consider a message fully populated when all the
handlers for fetched elements are fired in a single message (i.e. UID,
FLAGS, INTERNALDATE, BODYSTRUCTURE and BODY). This may not be
sufficient to guarantee that the message is indeed populated with all
required headers.

Additionally, a message was always considered to be populated when the
IMAP backend notifies the frontend of an update. We instead listen to
the message's own determination of whether it is populated or not.

src/imap/fetch.c
@@ -291,6 +291,7 @@ void handle_imap_fetch(struct imap_connection *imap, const char *token,
 		{ "BODY", IMAP_RESPONSE, handle_body },
 		{ "BODYSTRUCTURE", IMAP_LIST, handle_bodystructure },
 	};
+	bool handled[sizeof(handlers) / sizeof(handlers[0])] = { false };
 
 	while (args) {
 		const char *name = args->str;
@@ -302,6 +303,7 @@ void handle_imap_fetch(struct imap_connection *imap, const char *token,
 			if (strcmp(handlers[i].name, name) == 0) {
 				assert(args->type == handlers[i].expected_type);
 				int j = handlers[i].handler(msg, args);
+				handled[i] = true;
 				while (j-- && args) args = args->next;
 			}
 		}
@@ -310,7 +312,13 @@ void handle_imap_fetch(struct imap_connection *imap, const char *token,
 		}
 	}
 	msg->fetching = false;
+
 	msg->populated = true;
+	for (size_t i = 0; i < sizeof(handled) / sizeof(handled[0]); ++i) {
+		worker_log(L_DEBUG, "%s was %shandled", handlers[i].name, handled[i] ? "" : "not ");
+		msg->populated &= handled[i];
+	}
+
 	if (imap->events.message_updated) {
 		imap->events.message_updated(imap, msg);
 	}

src/handlers.c
@@ -163,7 +163,6 @@ void handle_worker_message_updated(struct account_state *account,
 		struct aerc_message *old = mbox->messages->items[i];
 		if (old->index == new->index) {
 			free_aerc_message(mbox->messages->items[i]);
-			new->fetched = true;
 			mbox->messages->items[i] = new;
 			rerender_item(i);
 			if (account->viewer.msg == old) {

commit 12f279d8ce8a4c2be66774bbd40019449fafcaf2

Preserve populated flag of IMAP messages
5736cb5 ("Handle flag change on unfetched messages", 2017-06-09) broke
the ability to view messages, by considering messages as depopulated
when handling the partial IMAP FETCH message for only the BODY
content. If a message is populated, it shouldn't be allowed to
re-enter the depopulated state.

src/imap/fetch.c
@@ -313,10 +313,16 @@ void handle_imap_fetch(struct imap_connection *imap, const char *token,
 	}
 	msg->fetching = false;
 
-	msg->populated = true;
-	for (size_t i = 0; i < sizeof(handled) / sizeof(handled[0]); ++i) {
-		worker_log(L_DEBUG, "%s was %shandled", handlers[i].name, handled[i] ? "" : "not ");
-		msg->populated &= handled[i];
+	// A partial FETCH message for an unpopulated message doesn't populate it
+	// but it doesn't depopulate an already populated message -- e.g. fetching
+	// the BODY of a message
+	if (!msg->populated) {
+		msg->populated = true;
+		for (size_t i = 0; i < sizeof(handled) / sizeof(handled[0]); ++i) {
+			worker_log(L_DEBUG, "%s was %shandled", handlers[i].name,
+				   handled[i] ? "" : "not ");
+			msg->populated &= handled[i];
+		}
 	}
 
 	if (imap->events.message_updated) {